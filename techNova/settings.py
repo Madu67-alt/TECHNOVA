2efdf43cd3f37f7885b3dfcf309413cdc77e6e6c
PROGRAMAÇÃO EM PYTHON - 2024.4 <47129532024.4@senacrjedu.local>
2024-12-13 11:26:08 -0300

    primeiro

--- techNova/settings.py
+++ techNova/settings.py
@@ -78,7 +78,12 @@ WSGI_APPLICATION = 'techNova.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':'',
+        'USER':'',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':'',
+        
     }
 }
 
